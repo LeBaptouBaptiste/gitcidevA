2969748f6a9f0e5af4967407b4f029e5edef2c39
LeBaptouBaptiste <baptiste.vidal.2005@gmail.com>
2025-10-01 14:18:38 +0200

    Toggle task added

--- eval-git-ci-js-base/test/tasks.test.js
+++ eval-git-ci-js-base/test/tasks.test.js
@@ -1,4 +1,4 @@
-const { getTasks, reset, addTask } = require('../lib/tasks');
+const { getTasks, reset, addTask, toggleTask } = require('../lib/tasks');
 
 beforeEach(() => {
   reset();
@@ -12,4 +12,13 @@ test('addTask adds a new task', () => {
   const task = addTask('Test task');
   expect(task).toEqual({ id: 1, name: 'Test task', done: false });
   expect(getTasks()).toEqual([task]);
+});
+
+test('toggleTask toggles the done status', () => {
+  const task = addTask('Another task');
+  expect(task.done).toBe(false);
+  const toggledTask = toggleTask(task.id);
+  expect(toggledTask.done).toBe(true);
+  const toggledAgainTask = toggleTask(task.id);
+  expect(toggledAgainTask.done).toBe(false);
 });
